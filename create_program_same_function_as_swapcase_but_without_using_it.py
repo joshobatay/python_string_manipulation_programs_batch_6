4a8fe6299d269cd80d5698066665b7bdba5ffb5c
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:56:02 +0800

    pseudocode

---
+++ create_program_same_function_as_swapcase_but_without_using_it.py
@@ -0,0 +1,9 @@
+# swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# Ask user to enter a string
+
+# Initiate an empty string
+
+# Process the entered string to reverse the casing of each character
+
+# Output the string with reversed casing
