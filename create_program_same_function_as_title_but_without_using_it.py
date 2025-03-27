b5482a0209586a591468fed0d4b09e519cc69bb1
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 17:21:19 +0800

    pseudocode

---
+++ create_program_same_function_as_title_but_without_using_it.py
@@ -0,0 +1,13 @@
+# title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# Ask user to enter a string
+
+# Split the entered string into words
+
+# Initialize an empty string for result
+
+# Process the entered string to capitalize the first letter of each word
+
+# Remove the trailing spaces from the result
+
+# Output the string with the first letter of each word capitalized
