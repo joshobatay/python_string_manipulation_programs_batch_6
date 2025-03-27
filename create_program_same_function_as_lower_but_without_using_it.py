653490b428719f8172a2039bd950493eae8a32f2
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 15:47:47 +0800

    added input and initialized an empty string

--- create_program_same_function_as_lower_but_without_using_it.py
+++ create_program_same_function_as_lower_but_without_using_it.py
@@ -1,7 +1,12 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # Ask user to enter a string in upper case
+entered_string = input("Enter string in upper case: ")
+
+# Initialize an empty string
+result = ""
 
 # Process the entered string to convert all characters into lower case
+
 
 # Output the string in lower case
