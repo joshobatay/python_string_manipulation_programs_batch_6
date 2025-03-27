da9af9a6b125f39c084941712e8de95807933cbe
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 15:29:24 +0800

    added process and output

--- create_program_same_function_as_removeprefix_but_without_using_it.py
+++ create_program_same_function_as_removeprefix_but_without_using_it.py
@@ -4,6 +4,12 @@
 entered_string = input("Enter a string with characters at the beginning: ")
 
 # Initiate what string to be removed and process
+prefix = "www."
 
+if entered_string.startswith(prefix):
+    cleaned_string = entered_string[len(prefix):]
+else:
+    cleaned_string = entered_string
 
 # Output the string without the characters at the beginning
+print(f"Cleaned String:\n{cleaned_string}")
