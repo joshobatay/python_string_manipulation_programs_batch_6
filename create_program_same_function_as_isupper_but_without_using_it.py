6d373dfc80d98fb417964dac1b12ec11da7e2b4c
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:13:28 +0800

    revised code

--- create_program_same_function_as_isupper_but_without_using_it.py
+++ create_program_same_function_as_isupper_but_without_using_it.py
@@ -3,17 +3,12 @@
 # Ask user to enter a string
 entered_string = input("Enter a string: ")
 
-# Assumes the string is in upper case
-is_upper = True
-
 # Process entered string to check if it is in upper case
 for char in entered_string:
-    if "a" <= char <= "z": # If lower case letters are found
-        is_upper = False 
+    if "a" <= char <= "z":  # If lower case letters are found
+        # Output the result if the string is in upper case or not
+        print("The entered string is not in upper case")
         break
-
-# Output the result if the string is in upper case or not
-if is_upper:
-    print("The entered string is in upper case")
 else:
-    print("The entered string is not in upper case")
+    print("The entered string is in upper case")
+
