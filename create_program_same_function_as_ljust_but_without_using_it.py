b14796aed2623144b0e77a46702a7f3ce974e1dd
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:33:07 +0800

    pseudocode

---
+++ create_program_same_function_as_ljust_but_without_using_it.py
@@ -0,0 +1,9 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Ask user to enter a string
+
+# Ask user width of the string
+
+# Process the entered string to add spaces at the end to complete the width
+
+# Output the string with spaces at the end to complete the width
