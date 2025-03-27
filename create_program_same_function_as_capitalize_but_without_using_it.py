d63c1c24dfacbc30f2f7f616a738911ff605540a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 17:08:18 +0800

    pseudocode

---
+++ create_program_same_function_as_capitalize_but_without_using_it.py
@@ -0,0 +1,7 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# Ask user to enter a string
+
+# Process entered string to capitalize the first letter
+
+# Output the string with the first letter capitalized
