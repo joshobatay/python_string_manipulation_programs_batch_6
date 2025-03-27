7f47bede3cf25b3990b33f4798e544d257c0de10
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:20:31 +0800

    added input

--- create_program_same_function_as_endswith_but_without_using_it.py
+++ create_program_same_function_as_endswith_but_without_using_it.py
@@ -1,9 +1,12 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # Ask user to input a string
+entered_string = input("Enter a string: ")
 
 # Ask user what string to check if it ends with
+suffix = input("Enter a string to check if the entered string ends with: ")
 
 # Process the entered string to check if it ends with the parameter string
+
 
 # Output the result if the string ends with the parameter string or not
