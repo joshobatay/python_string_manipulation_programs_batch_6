d58e3ea8e46292a105e62ba389ea2d44534c4b6b
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:00:38 +0800

    added input and initialized is_upper = true to assume string is in uppercase

--- create_program_same_function_as_isupper_but_without_using_it.py
+++ create_program_same_function_as_isupper_but_without_using_it.py
@@ -1,7 +1,12 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
+
+# Assumes the string is in upper case
+is_upper =  True
 
 # Process entered string to check if it is in upper case
+
 
 # Output the result if the string is in upper case or not
