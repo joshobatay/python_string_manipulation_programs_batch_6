4b3563c89ef2456744ae4cda841b02abe868e626
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 17:01:00 +0800

    added process

--- create_program_same_function_as_swapcase_but_without_using_it.py
+++ create_program_same_function_as_swapcase_but_without_using_it.py
@@ -7,5 +7,12 @@ entered_string = input("Enter a string: ")
 result = ""
 
 # Process the entered string to reverse the casing of each character
-
+for char in entered_string:
+    if "A" <= char <= "Z":
+        # ord converts character to ASCII, and 32 is added to convert to lower case, and chr converts ASCII chars to strings
+        result += chr(ord(char) + 32) 
+        # 32 is subtracted to convert to upper case
+    elif "a" <= char <= "z":
+        result += chr(ord(char) - 32)
+    
 # Output the string with reversed casing
