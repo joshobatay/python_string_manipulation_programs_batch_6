94e2597835492e3780cc82316617d5ead39c4a44
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:47:20 +0800

    added input

--- create_program_same_function_as_center_but_without_using_it.py
+++ create_program_same_function_as_center_but_without_using_it.py
@@ -1,9 +1,12 @@
 # center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Ask user width of the string
+prefered_width = int(input("Enter width of the string: "))
 
 # Process the entered string to add spaces at the beginning and at the end 
+
 
 # Output the string with spaces at the beginning and at the end to print the string at the center
