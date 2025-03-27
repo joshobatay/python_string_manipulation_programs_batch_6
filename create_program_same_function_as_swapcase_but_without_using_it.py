945033070409cd3a4eea5057e12f6e424f89e6e9
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:56:51 +0800

    added input and initialized an empty string

--- create_program_same_function_as_swapcase_but_without_using_it.py
+++ create_program_same_function_as_swapcase_but_without_using_it.py
@@ -1,8 +1,10 @@
 # swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Initiate an empty string
+result = ""
 
 # Process the entered string to reverse the casing of each character
 
