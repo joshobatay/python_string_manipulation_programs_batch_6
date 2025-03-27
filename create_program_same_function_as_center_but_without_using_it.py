573aefbb5c8748d097095db23893005995b37fa4
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:46:14 +0800

    pseudocode

---
+++ create_program_same_function_as_center_but_without_using_it.py
@@ -0,0 +1,9 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Ask user to enter a string
+
+# Ask user width of the string
+
+# Process the entered string to add spaces at the beginning and at the end 
+
+# Output the string with spaces at the beginning and at the end to print the string at the center
