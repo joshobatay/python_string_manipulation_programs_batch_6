d217bb9c9db6770e07fd1b0d5ddb98f13fa32b81
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 17:26:39 +0800

    added input and much more

--- create_program_same_function_as_title_but_without_using_it.py
+++ create_program_same_function_as_title_but_without_using_it.py
@@ -1,13 +1,18 @@
 # title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Split the entered string into words
+words = entered_string.split()
 
 # Initialize an empty string for result
+result = ""
 
 # Process the entered string to capitalize the first letter of each word
 
+     
 # Remove the trailing spaces from the result
+
 
 # Output the string with the first letter of each word capitalized
