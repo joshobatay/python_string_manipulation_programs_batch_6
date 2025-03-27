75735d5c755313ef8c71e605613ced97cac1dc07
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 15:44:43 +0800

    pseudocode

---
+++ create_program_same_function_as_lower_but_without_using_it.py
@@ -0,0 +1,7 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Ask user to enter a string in upper case
+
+# Process the entered string to convert all characters into lower case
+
+# Output the string in lower case
