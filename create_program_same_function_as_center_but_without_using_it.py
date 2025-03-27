2c5d537f44f9555df618cc6fb6ce767060de9da1
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:52:27 +0800

    made minor changes, added more comments and added output

--- create_program_same_function_as_center_but_without_using_it.py
+++ create_program_same_function_as_center_but_without_using_it.py
@@ -4,12 +4,17 @@
 entered_string = input("Enter a string: ")
 
 # Ask user width of the string
-prefered_width = int(input("Enter width of the string: "))
+preferred_width = int(input("Enter width of the string: "))
 
 # Process the entered string to add spaces at the beginning and at the end 
-total_padding = max(0, prefered_width - len(entered_string)) # max for ensuring padding is not negative
+total_padding = max(0, preferred_width - len(entered_string)) # max for ensuring padding is not negative
+
+# Distributes padding evenly on both sides
 padding_left = total_padding // 2
 padding_right = total_padding - padding_left
+
+# Constructs the modified string
 modified_string = " " * padding_left + entered_string + " " * padding_right
 
 # Output the string with spaces at the beginning and at the end to print the string at the center
+print(f"Modified String:\n|{modified_string}|") # Straight lines to visualize added spaces at the beginning and at the end
