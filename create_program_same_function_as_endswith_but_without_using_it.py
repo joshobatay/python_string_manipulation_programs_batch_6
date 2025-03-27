be970808cd656cbfb368ef4f091919c55ac7ebfb
Josh Obatay <joshobatay2005@gmail.com>
2025-03-27 16:19:48 +0800

    pseudocode

---
+++ create_program_same_function_as_endswith_but_without_using_it.py
@@ -0,0 +1,9 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# Ask user to input a string
+
+# Ask user what string to check if it ends with
+
+# Process the entered string to check if it ends with the parameter string
+
+# Output the result if the string ends with the parameter string or not
