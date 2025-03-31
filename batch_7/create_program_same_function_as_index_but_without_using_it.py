b209f380097f08957d4199982e4c2105fd383918
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:54:52 +0800

    added the inputs and initialized a variable for the storage of index

--- batch_7/create_program_same_function_as_index_but_without_using_it.py
+++ batch_7/create_program_same_function_as_index_but_without_using_it.py
@@ -1,11 +1,15 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Ask user a character to find its index
+char_index = input("Enter a character to find its index: ")
 
 # Initialize a variable to store the index
+index = -1
 
 # Process the entered string to find the index of the character
+
 
 # Output the index of the character in the string
