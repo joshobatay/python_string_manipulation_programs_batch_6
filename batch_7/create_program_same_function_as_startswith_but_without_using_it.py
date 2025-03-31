4370128a13c2a6f076dec67a43bc00be0ff3240a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:42:58 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_startswith_but_without_using_it.py
@@ -0,0 +1,7 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# Ask user to enter a string
+
+# Process entered string to check if it starts with the parameter string
+
+# Output the result if the string starts with the parameter string or not
