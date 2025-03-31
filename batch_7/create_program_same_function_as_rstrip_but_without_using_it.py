64f2fca798dafc4160373be80984c2bd8e61a2c5
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:23:09 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_rstrip_but_without_using_it.py
@@ -0,0 +1,7 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# Import built in module textwrap
+
+# Process the entered string by reversing it, removing spaces at the beginning, and then reversing it back
+
+# Output the string without spaces at the end
