11d22e8215270af7f538296c4c845e5f477538e6
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:56:48 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_rjust_but_without_using_it.py
@@ -0,0 +1,9 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# Ask user to enter a string
+
+# Ask user preferred width of the string
+
+# Process the entered string to add spaces at the beginning to complete the width
+
+# Output the string with spaces at the beginning  
