71650cd08dfddd6dcf7d4135b378c9a5b51fa857
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 20:11:05 +0800

    added process and output, code is working properly

--- batch_7/create_program_same_function_as_rindex_but_without_using_it.py
+++ batch_7/create_program_same_function_as_rindex_but_without_using_it.py
@@ -10,5 +10,13 @@ search_char = input("Enter a character to find its index: ")
 index_pos = -1
 
 # Process the entered string to find the index of the character 
+for i in range(len(entered_string) - 1, -1, -1):
+    if entered_string[i] == search_char:
+        index_pos = i
+        break
 
 # Output the index of the character in the string
+if index_pos != -1:
+    print(f"The character '{search_char}' is found at index {index_pos} in the string.")
+else:
+    print(f"The character '{search_char}' is not found in the string.")
