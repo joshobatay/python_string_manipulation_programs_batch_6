05413e68b1534e89e4504a5050dc1560ebc14788
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:49:03 +0800

    added process and output, the code is complete and fully working

--- batch_7/create_program_same_function_as_count_but_without_using_it.py
+++ batch_7/create_program_same_function_as_count_but_without_using_it.py
@@ -10,5 +10,9 @@ char_count = input("Enter a character to count: ")
 counter = 0
 
 # Process the entered string to count the occurrences of the character
+for char in entered_string:
+    if char == char_count:
+        counter += 1
 
 # Output the number of occurrences of the character in the string
+print(f"The character '{char_count}' appears {counter} times in the string.")
