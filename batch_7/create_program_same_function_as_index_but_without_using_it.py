38cf9abcead5da1618cec91a5eefa618b9b13242
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:53:04 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_index_but_without_using_it.py
@@ -0,0 +1,11 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# Ask user to enter a string
+
+# Ask user a character to find its index
+
+# Initialize a variable to store the index
+
+# Process the entered string to find the index of the character
+
+# Output the index of the character in the string
