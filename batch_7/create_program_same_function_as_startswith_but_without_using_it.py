cec5475c66e60b27a9938a1865e08988f0b29ae5
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:43:55 +0800

    added another input for checking prefix

--- batch_7/create_program_same_function_as_startswith_but_without_using_it.py
+++ batch_7/create_program_same_function_as_startswith_but_without_using_it.py
@@ -1,7 +1,12 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
+
+# Ask user what string to check if it starts with
+prefix = input("Enter a string to check if the entered string starts with: ")
 
 # Process entered string to check if it starts with the parameter string
+
 
 # Output the result if the string starts with the parameter string or not
