9c734bfc3319e776abd12296abe7948450dd2c13
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 20:08:34 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_rindex_but_without_using_it.py
@@ -0,0 +1,11 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# Ask user to enter a string
+
+# Ask user for a character to find its index
+
+# Initialize a variable to store the index
+
+# Process the entered string to find the index of the character 
+
+# Output the index of the character in the string
