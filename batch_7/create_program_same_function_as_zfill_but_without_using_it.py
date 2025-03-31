0d4e7970efb8f5216385b7253c064cfbb22eeaa4
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 16:20:45 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_zfill_but_without_using_it.py
@@ -0,0 +1,10 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# Ask user to enter a number
+
+# Ask user preferred digits
+
+# Process entered number to add zeros at the beginning to complete the digits
+
+# Output the string with zeros at the beginning to complete the digits
+
