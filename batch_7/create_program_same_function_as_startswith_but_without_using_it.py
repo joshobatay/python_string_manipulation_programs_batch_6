26681047a53359f944dfa8e0f05c12a49a4f8bed
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:52:25 +0800

    added process and output, code is fully working

--- batch_7/create_program_same_function_as_startswith_but_without_using_it.py
+++ batch_7/create_program_same_function_as_startswith_but_without_using_it.py
@@ -4,9 +4,10 @@
 entered_string = input("Enter a string: ")
 
 # Ask user what string to check if it starts with
-prefix = input("Enter a string to check if the entered string starts with: ")
+prefix = input("Enter a string to check: ")
 
 # Process entered string to check if it starts with the parameter string
-
-
-# Output the result if the string starts with the parameter string or not
+if entered_string[:len(prefix)] == prefix:
+    print(f"The entered string starts with the prefix: {prefix}") # Output the result
+else:
+    print(f"The entered string does not start with {prefix}")
