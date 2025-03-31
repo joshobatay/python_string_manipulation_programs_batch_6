64a5635daaa70555368740b2447b9a9225698ffb
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 16:24:14 +0800

    modified the entered_number input, added process and output

--- batch_7/create_program_same_function_as_zfill_but_without_using_it.py
+++ batch_7/create_program_same_function_as_zfill_but_without_using_it.py
@@ -1,12 +1,16 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # Ask user to enter a number
-entered_number = int(input("Enter a number: "))
+entered_number = input("Enter a number: ")
 
 # Ask user preferred digits
 preferred_digits = int(input("Enter preferred digits: "))
 
 # Process entered number to add zeros at the beginning to complete the digits
+if len(entered_number) < preferred_digits:
+    modified_string = "0" * (preferred_digits - len(entered_number)) + entered_number
+else:
+    modified_string = entered_number
 
 # Output the string with zeros at the beginning to complete the digits
-
+print(modified_string)
