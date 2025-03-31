30ceef4c848f77212f9835be7f2841f712127819
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:15:40 +0800

    added input

--- batch_7/create_program_same_function_as_removesuffix_but_without_using_it.py
+++ batch_7/create_program_same_function_as_removesuffix_but_without_using_it.py
@@ -1,6 +1,7 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Process entered string to remove the suffix
 
