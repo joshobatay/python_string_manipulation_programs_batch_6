e8d98e6181b8c1f4acdb56d644d7a47985093618
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:34:28 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_islower_but_without_using_it.py
@@ -0,0 +1,7 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+# Ask user to enter a string
+
+# Process entered_string to check if it is lower case
+
+# Output the result
