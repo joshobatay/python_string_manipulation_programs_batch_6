e6418e7f3fd99be0326c2f4a2e7cbe85a9be9b70
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:18:36 +0800

    added additional input for the suffix to be removed

--- batch_7/create_program_same_function_as_removesuffix_but_without_using_it.py
+++ batch_7/create_program_same_function_as_removesuffix_but_without_using_it.py
@@ -3,6 +3,12 @@
 # Ask user to enter a string
 entered_string = input("Enter a string: ")
 
+# Ask user to enter a suffix to be removed
+suffix = input("Enter a suffix to be removed: ")
+
 # Process entered string to remove the suffix
+
+if entered_string[-len(suffix):] == suffix:
+    modified_string = entered_string[:-len(suffix)]
 
 # Output the string with the suffix removed
