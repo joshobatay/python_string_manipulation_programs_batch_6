8dc986eed8d62c65862bacd75ddbc75582bc6d2d
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:47:05 +0800

    added the inputs and initialized a counter

--- batch_7/create_program_same_function_as_count_but_without_using_it.py
+++ batch_7/create_program_same_function_as_count_but_without_using_it.py
@@ -1,10 +1,13 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # Ask user to enter a string
+entered_string = input("Enter a string: ")
 
 # Ask user character to count
+char_count = input("Enter a character to count: ")
 
 # Initialize a counter variable to zero
+counter = 0
 
 # Process the entered string to count the occurrences of the character
 
