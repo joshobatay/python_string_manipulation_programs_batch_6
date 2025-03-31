df49bff7a0716609fc0d1ee64883d1a5cdced100
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 16:26:06 +0800

    added conditions if a negative number is entered

--- batch_7/create_program_same_function_as_zfill_but_without_using_it.py
+++ batch_7/create_program_same_function_as_zfill_but_without_using_it.py
@@ -6,11 +6,22 @@ entered_number = input("Enter a number: ")
 # Ask user preferred digits
 preferred_digits = int(input("Enter preferred digits: "))
 
+# Checks if entered number is negative
+is_negative = entered_number.startswith('-')
+
+# Remove the negative sign for processing
+if is_negative:
+    entered_number = entered_number[1:]
+
 # Process entered number to add zeros at the beginning to complete the digits
 if len(entered_number) < preferred_digits:
     modified_string = "0" * (preferred_digits - len(entered_number)) + entered_number
 else:
     modified_string = entered_number
+
+# Restore the negative sign if it was removed
+if is_negative:
+    modified_string = '-' + modified_string
 
 # Output the string with zeros at the beginning to complete the digits
 print(modified_string)
