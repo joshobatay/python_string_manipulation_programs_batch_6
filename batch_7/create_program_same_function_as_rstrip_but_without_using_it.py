ef24fc8aface222c20b99df7d4b8a94a3a7f9c00
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:26:28 +0800

    importted module and added input

--- batch_7/create_program_same_function_as_rstrip_but_without_using_it.py
+++ batch_7/create_program_same_function_as_rstrip_but_without_using_it.py
@@ -1,6 +1,10 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # Import built in module textwrap
+import textwrap
+
+# Ask user to enter a string with spaces at the end
+entered_string = input("Enter a string with spaces at the end: ")
 
 # Process the entered string by reversing it, removing spaces at the beginning, and then reversing it back
 
