7650c3d93ec296d772e9927c145a4281d55ab923
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 19:46:03 +0800

    pseudocode

---
+++ batch_7/create_program_same_function_as_count_but_without_using_it.py
@@ -0,0 +1,11 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# Ask user to enter a string
+
+# Ask user character to count
+
+# Initialize a counter variable to zero
+
+# Process the entered string to count the occurrences of the character
+
+# Output the number of occurrences of the character in the string
