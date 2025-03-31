edb3b35917f5b2e06992066a158c71d004ee3659
Josh Obatay <joshobatay2005@gmail.com>
2025-03-31 15:32:56 +0800

    added process and output, code is complete and fully working

--- batch_7/create_program_same_function_as_upper_but_without_using_it.py
+++ batch_7/create_program_same_function_as_upper_but_without_using_it.py
@@ -1,11 +1,18 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # Ask user to enter a string in lower case
-entered_string = input("Enter a string in lower case:")
+entered_string = input("Enter a string in lower case: ")
 
 # Initialize an empty string
 result = ""
 
 # Process entered string to convert all characters into upper case
+for char in entered_string:
+    if "a" <= char <= "z":
+        # ord converts character to ASCII, and 32 is subtracted to convert to upper case, and chr converts ASCII chars to strings
+        result += chr(ord(char) - 32)
+    else:
+        result += char # Does not change the character if it is not a lower case letter
 
 # Output the string in upper case
+print(result)
